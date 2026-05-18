230b4229eb0fbc8de46d125efd78dd2becd0c737
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-05-18 19:35:06 +0530

    Refactor prime number logic in prime.py
    Refactored prime number generation logic to simplify the process of finding prime numbers up to a given number.

--- prime.py
+++ prime.py
@@ -5,17 +5,25 @@
 # else:
 #     print("Prime number")
 
-num = int(input("Enter the range:"))
-if num < 2:
-    print("There are no prime numbers in this range.")
-else:
-    primes = []
-    for i in range(2, num + 1):
-        is_prime = True
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
-    print("Prime numbers in the range:", primes)
+# num = int(input("Enter the range:"))
+# if num < 2:
+#     print("There are no prime numbers in this range.")
+# else:
+#     primes = []
+#     for i in range(2, num + 1):
+#         is_prime = True
+#         for j in range(2, int(i**0.5) + 1):
+#             if i % j == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime:
+#             primes.append(i)
+#     print("Prime numbers in the range:", primes)
+
+n=int(input())
+for i in range(2,n):
+    for j in range(2,i):
+        if (i%j)==0:
+            break
+    else:
+        print(i)
